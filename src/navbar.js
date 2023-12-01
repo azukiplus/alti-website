f0aa2e8da9d38a7a7596dad5e99d6cf43ac892b0
Ashley Mochizuki <azuki@uw.edu>
2023-11-30 19:02:32 -0800

    clean up imports

--- src/navbar.js
+++ src/navbar.js
@@ -1,12 +1,5 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import InputGroup from "react-bootstrap/esm/InputGroup";
+import React from 'react';
+import {Container, Nav, Navbar, NavDropdown, Row, Col, Form, Button, InputGroup} from 'react-bootstrap';
 
 function NavBar() {
     return (
