39c304d0ce331e327326ab184a0b7946b103ebd6
Keejay Kim <keejayk@cs.washington.edu>
2023-11-30 18:31:55 -0800

    add discord button on navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,43 +1,47 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/esm/InputGroup";
 
 function NavBar() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="/">alti</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <NavDropdown title="Resources" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/resources/getting-started">Getting Started</NavDropdown.Item>
-                            <NavDropdown.Item href="/resources/faq">
+        <Navbar expand='lg' className='bg-body-tertiary'>
+            <Container fluid>
+                <Navbar.Brand href='/'>alti</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <NavDropdown title='Resources' id='basic-nav-dropdown'>
+                            <NavDropdown.Item href='/resources/getting-started'>
+                                Getting Started
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href='/resources/faq'>
                                 Frequently Asked Questions
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href='/about'>About</Nav.Link>
                     </Nav>
+                    <div className='d-flex gap-3'>
+                        <Form inline>
+                            <InputGroup>
+                                <Form.Control
+                                    type='text'
+                                    placeholder='Search'
+                                />
+                                <Button type='submit'>Submit</Button>
+                            </InputGroup>
+                        </Form>
+                        <Button type='submit'>
+                            Add to Discord
+                        </Button>
+                    </div>
                 </Navbar.Collapse>
-                <Form inline>
-                <Row>
-                    <Col xs="auto">
-                        <Form.Control
-                            type="text"
-                            placeholder="Search"
-                            className=" mr-sm-2"
-                        />
-                    </Col>
-                    <Col xs="auto">
-                        <Button type="submit">Submit</Button>
-                    </Col>
-                </Row>
-            </Form>
             </Container>
         </Navbar>
     );
