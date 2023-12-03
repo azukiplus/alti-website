d7e9f0b6639c7734faf67958e692fba296a93e87
azukiplus <ashleymochizuki@gmail.com>
2023-12-02 22:12:08 -0800

    add interactivity to the burger image

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Image, Button, Stack, Container, Row, Col } from 'react-bootstrap';
 import CarouselComp from '../carousel';
 import '../css/home.css';
 
 const HomePage = () => {
+  const [buntext, setBuntext] = useState('?');
+  const [meattext, setMeattext] = useState('?');
+  const [vegtext, setVegtext] = useState('?'); 
+  const [bagtext, setBagtext] = useState('?');
+
+  const bunClick = () => {
+    buntext === '?' ? setBuntext('A light brown, toasted hamburger bun.') : setBuntext('?');
+  }
+
+  const meatClick = () => {
+    meattext === '?' ? setMeattext('A hamburger patty with grill marks.') : setMeattext('?');
+  }
+
+  const vegClick = () => {
+    vegtext === '?' ? setVegtext('A set of green lettuce leaves, a red tomato slice, and purple sliced onions.') : setVegtext('?');
+  }
+
+  const bagClick = () => {
+    bagtext === '?' ? setBagtext('A brown paper bag.') : setBagtext('?');
+  }
+
   return (
     <>
       <div className="image-container">
@@ -30,15 +51,28 @@ const HomePage = () => {
         <Row style={{ marginRight: 0, marginLeft: 0 }}>
           <Col sm={8}>
             <Container className='p-4 example-img-container'>
-              <Image src={'img/burger.jpg'} alt="burger" fluid />
+              <Image src={'img/burger.jpg'} alt="burger" className="layer-parent" fluid />
+              <Button type='checkbox' className="layer-bun circular-btn" onClick={bunClick}>{buntext}</Button>
+              <Button type='checkbox' className="layer-meat circular-btn" onClick={meatClick}>{meattext}</Button>
+              <Button type='checkbox' className="layer-vegetables circular-btn" onClick={vegClick}>{vegtext}</Button>
+              <Button type='checkbox' className="layer-bag circular-btn" onClick={bagClick}>{bagtext}</Button>
+              <p className="img-caption pt-4">Click on the question marks for examples of alt text that describe that particular part of the image.</p>
             </Container>
           </Col>
           <Col sm={4}>
             <section className="me-3 pe-3">
-              <h3 className="pt-3 pb-3">What is alt text?</h3>
-              <p className="pt-3 pb-3">
+              <h3>What is alt text?</h3>
+              <p>
                 Alt text, shortened from the term alternative text, is text that gets read out loud by screen readers. Screen readers are used by people who are blind or have low vision to navigate the web. Alt text is used to describe images, so that people who are blind or have low vision can understand what the image is about.
               </p>
+              <h4>Good alt text v.s. bad alt text</h4>
+              <p>
+                Good alt text describes the image in as much detail as a user who is not blind or has low vision can understand. Bad alt text is either too vague or too specific. 
+              </p>
+                <ul>
+                  <li>Good alt text: "A lightly toasted burger bun with lettuce, purple onion slices, a tomato slice, and a grilled patty inside it. The burger is in a paper bag."</li>
+                  <li>Bad alt text: "A burger"</li>
+                </ul>
             </section>
           </Col>
         </Row>
