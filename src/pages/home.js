742931007491272031d0d5bd99a00c352116a0a3
Ashley Mochizuki <azuki@uw.edu>
2023-12-02 17:40:38 -0800

    fix padding bug

--- src/pages/home.js
+++ src/pages/home.js
@@ -27,19 +27,35 @@ const HomePage = () => {
       </Container>
 
       <div>
-        <Row>
+        <Row style={{ marginRight: 0, marginLeft: 0 }}>
           <Col sm={8}>
             <Container className='p-4 example-img-container'>
               <Image src={'img/burger.jpg'} alt="burger" fluid />
             </Container>
           </Col>
           <Col sm={4}>
-            <section className="mt-3 pb-3">
+            <section className="me-3 pe-3">
               <h3 className="pt-3 pb-3">What is alt text?</h3>
               <p className="pt-3 pb-3">
-                so this is a description that i will add later
+                Alt text, shortened from the term alternative text, is text that gets read out loud by screen readers. Screen readers are used by people who are blind or have low vision to navigate the web. Alt text is used to describe images, so that people who are blind or have low vision can understand what the image is about.
               </p>
             </section>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={4}>
+            <section className="ms-3 ps-3">
+              <h3 className="pt-3 pb-3 ms-3 ps-3">About alti</h3>
+              <p className="pt-3 pb-3 ms-3 ps-3">
+                Alti is a Discord bot developed by a team of students at the University of Washington, Seattle. Once you add Alti to your server, whenever someone sends an image, Alti will automatically generate alt text for the image using Artificial Intelligence. It then sends the alt text into the server as a message. Discord has a built-in way to add alt text to an image, but that requires the uploader to use the feature, which not everyone does. Alti makes it so that any image can be more accessible for users who are low vision or are blind.
+              </p>
+            </section>
+
+          </Col>
+          <Col sm={8}>
+            <Container className='p-4 example-img-container'>
+              <Image src={'img/burger.jpg'} alt="burger" fluid />
+            </Container>
           </Col>
         </Row>
       </div>
